3a190f631d7963d14f1759df27eb741a0adcf6ed
Senira Mendis <seniramendis41@gmail.com>
2026-03-22 17:25:13 +0530

    build the setup

---
+++ KMC.API/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using KMC.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KMC.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Registration> Registrations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // This prevents duplicate accounts by forcing emails to be unique
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+        }
+    }
+}
--- KMC.API/Program.cs
+++ KMC.API/Program.cs
@@ -1,3 +1,5 @@
+using KMC.API.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace KMC.API
 {
@@ -8,9 +10,12 @@ namespace KMC.API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+            // ADDED THIS: Database Configuration
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
@@ -26,7 +31,6 @@ namespace KMC.API
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
